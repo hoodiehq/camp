fd2f2160d57ccb50e025982241b60d55561a18e8
Gregor <gregor@martynus.net>
2016-09-11 09:25:58 -0700

    deploy

--- app/helpers/copyright.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function copyright() {
-  var year = new Date().getFullYear();
-  return `© 2012-${year}`;
-}
-
-export default Ember.Helper.helper(copyright);
--- app/routes/index.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  github: Ember.inject.service('github'),
-  model() {
-    return this.get('github').getCampIssues();
-  }
-});
